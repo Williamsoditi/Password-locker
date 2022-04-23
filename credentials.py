644e8473c605f4dcaab14a14913a737e20634c79
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-23 18:14:36 +0300

    Feat: Adds instance variables to the classes

--- credentials.py
+++ credentials.py
@@ -1,4 +1,15 @@
 class Credentials:
     '''
-    Class that generates instances of user credentials
+    Class that generates new instances of user credentials
     '''
+    def __init__(self,accountname,login_pass):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            accountname: Acount name 
+            login key: Account password
+        '''
+        self.accountname = accountname
+        self.login_key = login_pass
--- user.py
+++ user.py
@@ -2,3 +2,16 @@ class User:
     '''
     Class that generates new instances of users
     '''
+
+    def __init__(self,password, username):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+        Username: User name
+        Password: Account password
+        '''
+        
+        self.password = password
+        self.username = username
