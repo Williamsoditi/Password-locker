d52c8cb9a20c0149575a6d3a137dbcd0ed656669
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-23 19:17:37 +0300

    Feat : Save user object test - This adds a test to save user object

--- user.py
+++ user.py
@@ -15,3 +15,11 @@ class User:
         '''
         self.username = username
         self.password = password
+
+    def save_user(self):
+        
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)
+
