131236901fecf18ba8139af820e50ae542af0ce8
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-24 19:04:23 +0300

    Feat : Adds sign-up - This commit adds the sign-up method in run.py file

--- run.py
+++ run.py
@@ -58,6 +58,118 @@ def display_credentials():
     '''
     return Credentials.display_credentials()
 
+def main():
+    print('Welcome to PASSWORD-LOCKER !')
+    print('Please select an option to continue')
+    print('/n')
+    while True:
+        print("lg => to Login to your account")
+        print("sg => to Sign-up and create an account.")
+        short_form = input().lower()
+        print('\n')
+        if short_form == 'sg':
+                            print("Enter UserName")
+                            username = input()
+
+                            print ("Enter Password")
+                            password = input()
+                            
+                            print ("Confirm Password")
+                            confirm_password = input()
+
+                            while confirm_password != password:
+                                print("Error! Passwords don't match! Kindly try again")
+                                print("Enter a Password")
+                                password = input()
+                                print("Confirm Password")
+                                confirm_password = input()
+                            else :
+                                print(f" Hello {username}!. Welcome to Password-Locker. Your account was created successfully!")
+                                print('\n')
+                                print("Please login to continue")
+                                print("Enter Username")
+                                entered_username = input()
+                                print("Enter Password")
+                                entered_password = input()
+                            
+                            while entered_username != username or entered_password != password:
+                                print("Error! Invalid username or password. Please enter correct details")
+
+                                print("Enter your Username")
+                                entered_username = input()
+                                print("Enter your Password")
+                                entered_password = input()
+
+                            else:
+                                print(f"Welcome to your account @{username}")
+                                while True:
+                                    print("Please choose an option to continue")
+                                    print("cc => to create new credentials")
+                                    print("dc => to display credentials")
+                                    print("cp => to copy credential details to clipboard")
+                                    print("del => to delete account credentials")
+                                    print("ex => to exit the application")
+                                    short_form = input().lower()
+                                    if short_form == 'cc':
+                                        print("Enter account name")
+                                        acc_name = input()
+
+                                        while True:
+                                            print("Use the codes to choose an option")
+                                            print("el => to enter login key")
+                                            print("gl => for a system generated key")
+                                            print("ex => to exit the prompt")
+                                            l_key = input().lower()
+                                            print("\n")
+                                            if l_key == 'el':
+                                                print("Enter login key")
+                                                log_key = input()
+                                            elif l_key == 'gl':
+                                                log_key = generate_loginkey()
+                                                break
+                                            elif l_key == 'ex':
+                                                break
+                                            else:
+                                                print("Please check your input!")
+
+                                        save_credentials(create_credentials(acc_name,log_key))
+                                        print(f"Credentials for {acc_name} have been created and its login key is {log_key}")
+
+                                    elif short_form == 'dc':
+                                        if display_credentials():
+                                            print("Find a list of your credentials below")
+                                            print("\n")
+                                            for credentials in display_credentials():
+                                                print(f"{credentials.accountname} - {credentials.login_key}")
+                                        else:
+                                            print(":( You dont have any saved credentials yet!")
+
+                                    elif short_form == 'cp':
+                                        print("Enter account name whose credentials you want to copy!")
+                                        accountname = input()
+                                        account = find_credentials(accountname)
+                                        pyperclip.copy(account.accountname)
+
+                                    elif short_form == 'del':
+                                        print("Enter account name you wish to delete")
+                                        accountname = input()
+                                        account = find_credentials(accountname)
+                                        del_credentials(account)
+
+                                    elif short_form == 'ex':
+                                        print("Are you sure you want to EXIT !")
+                                        print("Y/N?")
+                                        answer = input().upper()
+                                        if answer == 'Y':
+                                            print("Your details will be autosaved for the next login!")
+                                            break
+                                        elif answer == 'N':
+                                            print("Please choose an option to continue:'cc' to create new credentials,'del' to delete account credentials,'dc' to display credentials,'cp' to copy credential details to clipboard, 'ex' to exit the application")
+                                            short_form = input().lower()
+                                        else:
+                                            print("Choose a valid option!")
+
+    
 
 
 if __name__ == '__main__':
